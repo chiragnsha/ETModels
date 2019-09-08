61d2d18c19c4e495f1daf09ea2ebde3d5009b511
Chirag <chirag.shah@zohocorp.com>
2019-09-08 15:03:00 +0530

    Initial Commit

---
+++ ETModels/ETModels.h
@@ -0,0 +1,19 @@
+//
+//  ETModels.h
+//  ETModels
+//
+//  Created by Chirag N Shah on 08/09/19.
+//  Copyright © 2019 Chirag N Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ETModels.
+FOUNDATION_EXPORT double ETModelsVersionNumber;
+
+//! Project version string for ETModels.
+FOUNDATION_EXPORT const unsigned char ETModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ETModels/PublicHeader.h>
+
+
