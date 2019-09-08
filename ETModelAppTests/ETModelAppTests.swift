48eb120825bc9cd4e059046aadfd3e35bb28692a
Chirag <chirag.shah@zohocorp.com>
2019-09-08 19:09:44 +0530

    added test case setup only

--- ETModelAppTests/ETModelAppTests.swift
+++ ETModelAppTests/ETModelAppTests.swift
@@ -8,20 +8,41 @@
 
 import XCTest
 @testable import ETModelApp
+@testable import ETModels
 
 class ETModelAppTests: XCTestCase {
-
+    
+    let user1 = User.init(ID: UUID.init(), name: "Random user name")
+    let user2 = User.init(ID: UUID.init(), name: "Random user name2")
+    
+    let expense = Expense.init(expenseName: "Expense Name", expenseAmount: Double.random(in: 0.0...1000.0), payee: user1, involvedUsers: Set.init([user1, user1]))
+    
     override func setUp() {
+        
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
+    }
+    
+    func testNetExpense() {
+        ////check net expense case... etc.etc..
+        
+        
+        
     }
 
     func testPerformanceExample() {
